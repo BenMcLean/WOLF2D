a9234b585f3b24d71e3a19e49880033fb8e3754e
Benjamin McLean <mclean.ben@gmail.com>
2019-10-12 22:18:38 -0500

    Trying to make WarpWriter render a Wolf3D wall

--- Game.cs
+++ Game.cs
@@ -1,6 +1,9 @@
 using Godot;
 using NScumm.Audio.OPL.Woody;
 using OPL;
+using System;
+using WarpWriter.View.Color;
+using WarpWriter.View.Render;
 using WOLF3D;
 
 public class Game : Node2D
@@ -21,13 +24,62 @@ public class Game : Node2D
         {
             Text = "Hello World!",
         });
-
         AddChild(new Sprite()
         {
             Texture = Assets.Textures[0],
             GlobalPosition = new Vector2(200, 200),
-            Scale = new Vector2(6, 6)
+            Scale = new Vector2(6, 6),
         });
+
+
+        ByteArrayRenderer renderer = new ByteArrayRenderer()
+        {
+            Width = 64,
+            Height = 64,
+            Color = new FlatVoxelColor()
+            {
+                Palette = WarpWriterFriendlyPalette(Assets.VSwap.Palette),
+            },
+        };
+        uint wallIndex = 0;
+        for (uint x = 0; x < 64; x++)
+            for (uint y = 0; y < 64; y++)
+                renderer.Rect((int)x, (int)y, 1, 1,
+                    WarpWriterFriendly(
+                    Assets.VSwap.Indexes[wallIndex][(63 - y) * 64 + x]
+                    )
+                    );
+
+        Godot.Image image = new Image();
+        image.CreateFromData((int)renderer.Width, (int)renderer.Height, false, Image.Format.Rgba8, renderer.Bytes);
+        ImageTexture it = new ImageTexture();
+        it.CreateFromImage(image, 0);
+
+        AddChild(new Sprite()
+        {
+            Texture = it,
+            GlobalPosition = new Vector2(600, 200),
+            Scale = new Vector2(6, 6),
+        });
+    }
+
+    public static uint[] WarpWriterFriendlyPalette(uint[] palette)
+    {
+        uint[] friendly = new uint[256];
+        for (uint i = 0; i < 256; i++)
+            friendly[i] = ReverseBytes(palette[WarpWriterFriendly((byte)i)]);
+        return friendly;
+    }
+
+    public static byte WarpWriterFriendly(byte unfriendly)
+    {
+        return (byte)(unfriendly - 1);
+    }
+
+    public static uint ReverseBytes(uint value)
+    {
+        return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |
+            (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
     }
 
     //  public override void _Process(float delta)
--- WOLF3DTest/WOLF2DTest.cs
+++ WOLF3DTest/WOLF2DTest.cs
@@ -1,134 +1,15 @@
 ﻿using System;
-using System.IO;
-using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WOLF3D;
 
 namespace WOLF3DTest
 {
     [TestClass]
     public class WOLF2DTest
     {
-        public static readonly string Folder = @"..\..\..\WOLF3D\";
-        public static readonly XElement XML = Assets.LoadXML(Folder);
-
         [TestMethod]
-        public void VSwapTest()
+        public void TestMethod1()
         {
-            DownloadShareware.Main(new string[] { Folder });
-            VSwap vSwap = VSwap.Load(Folder, XML);
-            Console.WriteLine("Number of graphics pages: " + vSwap.Pages.Length.ToString());
-            Console.WriteLine("Number of DigiSounds: " + vSwap.DigiSounds.Length.ToString());
-        }
-
-        [TestMethod]
-        public void GameMapsTest()
-        {
-            DownloadShareware.Main(new string[] { Folder });
-            GameMap[] maps = GameMap.Load(Folder, XML);
-
-            GameMap map = maps[0];
-            Console.WriteLine(map.Name + ": ");
-            for (uint i = 0; i < map.MapData.Length; i++)
-            {
-                Console.Write(map.MapData[i].ToString("D3"));
-                if (i % map.Width == map.Width - 1)
-                    Console.WriteLine();
-                else
-                    Console.Write(" ");
-            }
-        }
-
-        [TestMethod]
-        public void VgaGraphTest()
-        {
-            DownloadShareware.Main(new string[] { Folder });
-            VgaGraph vgaGraph = VgaGraph.Load(Folder, XML);
-
-            if (vgaGraph.Sizes != null)
-            {
-                Console.Write("Image sizes: ");
-                foreach (ushort[] size in vgaGraph.Sizes)
-                    if (size != null)
-                        Console.Write("(" + size[0].ToString() + ", " + size[1].ToString() + ") ");
-                Console.WriteLine();
-            }
-
-            //uint chunk = 0;
-            //Console.Write("Chunk " + chunk.ToString() + " contents: ");
-            //foreach (byte bite in vgaGraph.File[chunk])
-            //    Console.Write(bite.ToString() + ", ");
-            //Console.WriteLine();
-        }
-
-        [TestMethod]
-        public void LengthsTest()
-        {
-            DownloadShareware.Main(new string[] { Folder });
-            uint[] head;
-            using (FileStream vgaHead = new FileStream(System.IO.Path.Combine(Folder, XML.Element("VgaGraph").Attribute("VgaHead").Value), FileMode.Open))
-                head = VgaGraph.ParseHead(vgaHead);
-            uint[] lengths = new uint[head.Length - 1];
-            using (FileStream vgaGraphStream = new FileStream(System.IO.Path.Combine(Folder, XML.Element("VgaGraph").Attribute("VgaGraph").Value), FileMode.Open))
-            using (BinaryReader binaryReader = new BinaryReader(vgaGraphStream))
-                for (uint i = 0; i < lengths.Length; i++)
-                {
-                    vgaGraphStream.Seek(head[i], 0);
-                    lengths[i] = binaryReader.ReadUInt32();
-                }
-
-            Console.WriteLine("Lengths from start of each chunk: ");
-            foreach (uint length in lengths)
-                Console.Write(length.ToString() + ", ");
-            Console.WriteLine();
-
-            Console.WriteLine("Sizes from chunk 0: ");
-            VgaGraph vgaGraph = VgaGraph.Load(Folder, XML);
-            foreach (ushort[] size in vgaGraph.Sizes)
-                Console.Write((size[0] * size[1]).ToString() + ", ");
-            Console.WriteLine();
-
-            Console.WriteLine("Pic sizes / 4: ");
-            foreach (byte[] pic in vgaGraph.Pics)
-                if (pic != null)
-                    Console.Write((pic.Length / 4).ToString() + ", ");
-            Console.WriteLine();
-        }
-
-        [TestMethod]
-        public void FontTest()
-        {
-            DownloadShareware.Main(new string[] { Folder });
-            VgaGraph vgaGraph = VgaGraph.Load(Folder, XML);
-            uint font = 0;
-            char letter = 'A';
-            Console.Write("Writing letter \"" + letter + "\":");
-            for (uint i = 0; i < vgaGraph.Fonts[font].Character[letter].Length; i++)
-            {
-                if (i % (vgaGraph.Fonts[font].Width[letter] * 4) == 0)
-                    Console.WriteLine();
-                Console.Write(
-                    vgaGraph.Fonts[font].Character[letter][i] == 0 ?
-                    "0"
-                    : "1"
-                    );
-            }
-            Console.WriteLine();
-
-            string str = "Ab";
-            Console.Write("Writing string \"" + str + "\":");
-            byte[] test = vgaGraph.Fonts[font].Line(str);
-            int width = vgaGraph.Fonts[font].CalcWidth(str) * 4;
-            for (uint i = 0; i < test.Length; i++)
-            {
-                if (i % width == 0)
-                    Console.WriteLine();
-                Console.Write(
-                    test[i] == 0 ?
-                    "0"
-                    : "1"
-                    );
-            }
+            Assert.AreEqual(Game.WarpWriterFriendly(255), 0);
         }
     }
 }
