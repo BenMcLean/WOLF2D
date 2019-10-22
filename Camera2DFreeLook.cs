f9d10bba11c05846d7580c5c438a7dc2c3d46a90
Benjamin McLean <mclean.ben@gmail.com>
2019-10-21 20:54:11 -0500

    Update Camera2DFreeLook.cs

--- Camera2DFreeLook.cs
+++ Camera2DFreeLook.cs
@@ -2,13 +2,31 @@ using Godot;
 
 public class Camera2DFreeLook : Camera2D
 {
-    //public override void _Ready()
-    //{
-
-    //}
+    public override void _Ready()
+    {
+        Current = true;
+    }
 
     public override void _Process(float delta)
     {
-        GlobalPosition += Vector2.Right * 100 * delta;
+        Position += InputDirection * (speed * delta);
     }
+
+    public static Vector2 InputDirection
+    {
+        get
+        {
+            return new Vector2(
+            Direction(Input.IsActionPressed("ui_right")) - Direction(Input.IsActionPressed("ui_left")),
+            Direction(Input.IsActionPressed("ui_down")) - Direction(Input.IsActionPressed("ui_up"))
+            );
+        }
+    }
+
+    public static int Direction(bool boolean)
+    {
+        return boolean ? 1 : -1;
+    }
+
+    public static readonly float speed = 1000f;
 }
